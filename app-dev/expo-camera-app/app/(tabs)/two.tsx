b2d94fd2b481b6954813633ed44adce55e3d309c
Brian Nguyen <brianphi.nguyen@gmail.com>
2023-12-01 17:01:44 +0100

    Can open camera now. But not take pictures

--- app-dev/expo-camera-app/app/(tabs)/two.tsx
+++ app-dev/expo-camera-app/app/(tabs)/two.tsx
@@ -1,58 +1,89 @@
-import { Pressable, StyleSheet, Button } from "react-native";
+import {
+  Pressable,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Text, View } from "../../components/Themed";
+import { Camera, CameraType } from "expo-camera";
 
 export default function TabTwoScreen() {
-	const [useNumber, setNumber] = useState(0);
-	const clicked = () => {
-		setNumber(useNumber + 1);
-	};
-	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>Open camera</Text>
-			<Text style={styles.title}>Amount clicked {useNumber}</Text>
-			<View
-				style={styles.separator}
-				lightColor="#eee"
-				darkColor="rgba(100,225,255,0.5)"
-			/>
-			<View className="flex p-5 m-12 rounded-md items-center justify-center bg-slate-500">
-				<Text>Implemented tailwind</Text>
-			</View>
-			<Pressable onPress={clicked}>
-				{({ pressed }) => (
-					<FontAwesome
-						name="camera"
-						size={25}
-						color="white"
-						style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-					/>
-				)}
-			</Pressable>
-			<Button
-				onPress={clicked}
-				title="Open camera"
-				color="#243584"
-				accessibilityLabel="Learn more about this blue button"
-			/>
-		</View>
-	);
+  const [type, setType] = useState(CameraType.back);
+
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [hasPermission, setHasPermission] = useState(null);
+  const cameraRef = useRef(null);
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  const takePicture = () => {
+    if (Camera) {
+      Camera.takePictureAsync().then(onPictureSaved);
+    }
+  };
+  function toggleCameraType() {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Camera
+        ratio="16:9"
+        useCamera2Api
+        type={type}
+        className="h-full w-full bg-slate-20"
+        ref={cameraRef}
+      >
+        <View>
+          <TouchableOpacity onPress={toggleCameraType}>
+            <Text>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity
+            className="bg-red-500 w-16 h-10"
+            onPress={async () => {
+              if (cameraRef.current) {
+                let photo = await cameraRef.current.takePictureAsync();
+                console.log(photo.uri);
+              }
+            }}
+          />
+        </View>
+      </Camera>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: "center",
-		justifyContent: "flex-start",
-	},
-	title: {
-		fontSize: 20,
-		fontWeight: "bold",
-	},
-	separator: {
-		marginVertical: 30,
-		height: 1,
-		width: "80%",
-	},
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
 });
