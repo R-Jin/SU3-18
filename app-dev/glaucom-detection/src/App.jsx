0d9acd8b76fdb3cd1403a41c25f0222ae6f6485e
Brian Nguyen <brianphi.nguyen@gmail.com>
2023-12-04 12:15:39 +0100

    button and function to upload file

--- app-dev/glaucom-detection/src/App.jsx
+++ app-dev/glaucom-detection/src/App.jsx
@@ -1,29 +1,33 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Button } from "@nextui-org/react";
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Divider,
-  Link,
-  Image,
-} from "@nextui-org/react";
 
 import "./App.css";
 
 function App() {
+  const inputFile = useRef(null);
+  const onButtonClick = () => {
+    // `current` points to the mounted file input element
+    inputFile.current.click();
+  };
   return (
     <>
-      <div></div>
+      <div>
+        <input
+          type="file"
+          id="file"
+          ref={inputFile}
+          style={{ display: "none" }}
+        />
+      </div>
       <h1>Do I have glaucoma?</h1>
 
-      <div className="flex gap-4 items-center">
-        <Button size="sm" color="primary">
-          Small
+      <div className="flex flex-col gap-4 items-center justify-center">
+        <Button size="md" onClick={onButtonClick}>
+          Ladda upp bild
         </Button>
-        <Button size="md">Medium</Button>
-        <Button size="lg">Large</Button>
+        <Button size="md" disabled>
+          Ta bild
+        </Button>
       </div>
     </>
   );
