cf69d2734be870261c465adedfaa7a2b1be5b2ae
Brian Nguyen <brianphi.nguyen@gmail.com>
2023-12-04 11:58:21 +0100

    changed tailwind config to interact with nextui

--- app-dev/glaucom-detection/src/App.jsx
+++ app-dev/glaucom-detection/src/App.jsx
@@ -1,27 +1,29 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Button } from "@nextui-org/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Link,
+  Image,
+} from "@nextui-org/react";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <div></div>
       <h1>Do I have glaucoma?</h1>
-      <div className="bg-slate-500 rounded-md">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Ladda upp fil
-        </button>
-        <button onClick={() => setCount((count) => count + 1)}>Ta bild</button>
+
+      <div className="flex gap-4 items-center">
+        <Button size="sm" color="primary">
+          Small
+        </Button>
+        <Button size="md">Medium</Button>
+        <Button size="lg">Large</Button>
       </div>
     </>
   );
